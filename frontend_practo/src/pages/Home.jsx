becaa06ea9e31a3a8911fedfa34ee6bfb23a2f28
raj gupta <kumarraj0932@gmail.com>
2026-05-23 04:27:11 +0530

    fix: resolve case-sensitive import path for Banner component

--- frontend_practo/src/pages/Home.jsx
+++ frontend_practo/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Speciality_menu from '../components/speciality_menu'
 import TopDoctors from '../components/TopDoctors'
-import Banner from '../components/banner'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
